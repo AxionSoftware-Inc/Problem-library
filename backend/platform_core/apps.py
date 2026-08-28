9b38fb3d1b218820acaa76e7c7fa7a7508c38ae5
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-28 22:14:20 +0500

    chore(ecosystem): remove unused platform core backend

--- backend/platform_core/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PlatformCoreConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "platform_core"
-    verbose_name = "Science Platform Core"
