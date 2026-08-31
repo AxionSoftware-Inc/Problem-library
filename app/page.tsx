772e536e486a498de7de77150d6aaede0d25fcb7
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-31 10:08:28 +0500

    Use premium Science landing directly

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import { LandingHero } from "@/components/home/landing-hero";
-import { PageShell } from "@/components/ui/page-shell";
 
 export default function Home() {
-  return (
-    <PageShell>
-      <LandingHero />
-    </PageShell>
-  );
+  return <LandingHero />;
 }
